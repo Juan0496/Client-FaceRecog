a488ca62dd8a5dac870f4204ae9be711670ce52b
Juan <miguel.9604.m@gmail.com>
2024-07-21 16:47:04 -0500

    actualizacion 2 dep

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Proces />
       <footer >
         <div className='titulo-foot'>
-          <h1 className='text3'>Sobre Covered Face Detector App</h1>
+          <h1 className='text3'>Sobre Covered Face Detector</h1>
       </div>
         <div className='sobre-app'>         
           <div className='descripcion-proy'>
@@ -18,7 +18,7 @@ function App() {
             <p>Junto a Vite, para la elaboración de la interfaz de usuario.</p>
             <h3> Fast Api:</h3>
             <p>Sirvio para la creacion de una API, que establece la comunicación con el servidor, quien procesará la imagen mediante una red neuronal convolucional;
-              la cuál, se entrenó previamente con el fin de reconocer tres estados de imagen; en primer lugar, una imagen con un rostro descubierto; en segundo lugar, un rostro cubierto con mascarilla;
+              la cuál, entrené previamente con el fin de reconocer tres estados de imagen; en primer lugar, una imagen con un rostro descubierto; en segundo lugar, un rostro cubierto con mascarilla;
               y por ultimo, un rostro cubierto con un casco.
             </p>
             <ul>Para el entrenamiento y procesamiento de las imágenes, se utilizaron las siguientes librerias de python.</ul>
@@ -38,7 +38,7 @@ function App() {
             <div className='mi-mensaje'> 
             <h2 className='text4'>Sobre el desarrollador:</h2>
             <p className='text4'>
-              Mi nombre Juan Moreno, soy mecatrónico de profesión, y especializado en desarrollo IOT; elaboré este proyecto,
+              Mi nombre es Juan Moreno, soy mecatrónico de profesión, y especializado en desarrollo IOT; elaboré este proyecto,
               con el fin de poner en práctica los conocimientos que adquirí sobre desarrollo web, procesamiento de imágenes e inteligencia
               artificial.     
             </p>
